6a48b3978e6805caeaee2cdf192dbf8dae06e060
Olga Zyk <my.agleh@gmail.com>
2019-02-07 15:13:47 +0200

    Add Flask-Bootstrap; Add routes for login and signup; Add debug mode to app.run();

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from database.models import *
 
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 
 def create_app():
+    Bootstrap(app)
     with app.app_context():
         db.init_app(app)
 
@@ -17,6 +19,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    app.run(debug=True)
